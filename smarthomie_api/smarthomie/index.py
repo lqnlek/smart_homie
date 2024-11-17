66257b62781cb04f929503e1246b87e371596588
André Camacho <andrecamacho@me.com>
2024-11-17 16:21:40 +0100

    more changes

--- smarthomie_api/smarthomie/index.py
+++ smarthomie_api/smarthomie/index.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 
-@app.route("/")
+books = [
+    { 'name': 'Harry Potter', 'amount': 1 },
+    { 'name': 'Lord of the Rings', 'amount': 12 }
+]
+
+@app.route('/')
 def hello_world():
-    return "Hello, World!"
+    return "T'es pas au bon endroit, connard!"
+
+@app.route('/books')
+def get_incomes():
+    return jsonify(books)
+
+
+@app.route('/books', methods=['POST'])
+def add_income():
+    books.append(request.get_json())
+    return '', 204
